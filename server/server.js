70d650a57271c3ec12ec92b45c011dcd3ee20cd1
Karthik Macherla <kmacher@seas.upenn.edu>
2021-11-22 14:51:32 -0500

    queries no impl

--- server/server.js
+++ server/server.js
@@ -10,26 +10,47 @@ const app = express();
 // Route 1 - register as GET 
 app.get('/hello', routes.hello)
 
-// Route 2 - register as GET 
-app.get('/jersey/:choice', routes.jersey)
+// Simple Routes based on Simple Queries
 
-// Route 3 - register as GET 
-app.get('/matches/:league', routes.all_matches)
 
-// Route 4 - register as GET 
-app.get('/players', routes.all_players)
+// ROUTE 1 -- This query finds all restaurants of a certain category
+// Example category is: Lebanese, Chinese, Indian, American, etc.
+app.get('/restaurants/cuisine/:cuisine', routes.restaurant_cuisine)
 
-// Route 5 - register as GET 
-app.get('/match', routes.match)
+// ROUTE 2 -- This query finds all yelps in a specific postal area 
+app.get('/restaurants/location/:postal', routes.restaurant_postal)
 
-// Route 6 - register as GET 
-app.get('/player', routes.player)
+// ROUTE 3 -- This query finds the zipcodes sorted by average yelp price range
+app.get('/restaurants/zip', routes.restaurant_zip)
 
-// Route 7 - register as GET 
-app.get('/search/matches', routes.search_matches)
+// ROUTE 4 -- This query finds the airbnbs sorted by price/# beds
+app.get('/airbnbs/bed_price', routes.airbnb_bed_price)
 
-// Route 8 - register as GET 
-app.get('/search/players', routes.search_players)
+// ROUTE 5 -- This query best airbnb rating to price ratio, as customers, we want this ratio to be high!
+app.get('/airbnbs/rating_price', routes.airbnb_rating_price)
+
+// ROUTE 6 -- This query selects airbnbs ordered by average nearby restaurant reviews. 
+// It begins by finding a table with airbnbs joined on proximal yelp businesses, it then finds the 
+// averages and sorts.
+app.get('/airbnbs/restaurant_reviews', routes.airbnb_restaurant_reviews)
+
+// ROUTE 7 -- This Query filters Airbnbs by having a Grocery store nearby.
+app.get('airbnbs/grocery', routes.airbnb_grocery)
+
+// ROUTE 8 -- This query find the Airbnbs sorted by proximity to closest Cuisine restaurant. It begins with a subquery
+// to find Cuisine restaurants and then finds the closest Cuisine restaurent to each Airbnb and sorts the list 
+// by this distance. 
+// Example category is: Lebanese, Chinese, Indian, American, etc.
+app.get('airbnbs/restaurants_close/:cuisine', routes.airbnb_restaurant_close)
+
+// ROUTE 9 -- This query is to find the airbnb with the best host and most nearby restaurants, by count. It distinguishes best 
+// host by super_host and host_acceptance_rate and returns the count of restaurants. 
+app.get('airbnbs/best_overall', routes.airbnb_host_restaurants)
+
+// ROUTE 10 -- This query is to find the airbnb with at least 1 Indian restaurant and one Convenience Store OR Grocery nearby 
+// (within lat long diff of less than 62). It makes 3 subtables and finds the airbnbs that satisfy this condition, 
+// along with the count of the number of yelp offerings. 
+app.get('airbnbs/convenience_store_with_indian', routes.airbnb_convenience)
 
 
 app.listen(config.server_port, () => {
